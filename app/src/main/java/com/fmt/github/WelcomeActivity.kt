56301bfaf8e2006bb01c4102fd1a9692d3b594b5
fangmingtao <fangmingtao@cias.cn>
2019-08-03 13:33:59 +0800

    启动页svg动画实现

--- app/src/main/java/com/fmt/github/WelcomeActivity.kt
+++ app/src/main/java/com/fmt/github/WelcomeActivity.kt
@@ -5,16 +5,27 @@ import com.fmt.github.base.activity.BaseActivity
 import com.fmt.github.data.db.DBInstance
 import com.fmt.github.home.activity.HomeActivity
 import com.fmt.github.user.activity.LoginActivity
-import kotlinx.coroutines.delay
+import com.jaredrummler.android.widget.AnimatedSvgView
+import kotlinx.android.synthetic.main.activity_welcome.*
 import kotlinx.coroutines.launch
+
 
 class WelcomeActivity : BaseActivity() {
 
     override fun getLayoutId(): Int = R.layout.activity_welcome
 
     override fun initView() {
+        mAnimatedSvgView.setViewportSize(512f, 512f)
+        mAnimatedSvgView.setOnStateChangeListener {
+            if (it == AnimatedSvgView.STATE_FINISHED) {
+                checkIsLogin()
+            }
+        }
+        mAnimatedSvgView.start()
+    }
+
+    private fun checkIsLogin() {
         launch {
-            delay(1000)
             val userList = DBInstance.mAppDataBase.getUserDao().getAll()
             if (userList == null || userList.isEmpty()) {
                 go2Activity(LoginActivity::class.java)
@@ -30,4 +41,5 @@ class WelcomeActivity : BaseActivity() {
             finish()
         }
     }
+
 }
