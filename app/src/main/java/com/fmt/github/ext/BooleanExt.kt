949a55a66ef04d2996a3db24509561a8115b3e4d
fangmingtao <fangmingtao@cias.cn>
2020-02-03 16:14:49 +0800

    添加搜索仓库和搜索用户排序功能

--- app/src/main/java/com/fmt/github/ext/BooleanExt.kt
+++ app/src/main/java/com/fmt/github/ext/BooleanExt.kt
@@ -6,7 +6,7 @@ sealed class BooleanExt<out T>//巧用协变与密封类(增强版枚举)
 
 class Success<T>(val data: T) : BooleanExt<T>()
 
-object OtherWise : BooleanExt<Nothing>()
+object OtherWise : BooleanExt<Nothing>()//Nothing是任何类的子类
 
 inline fun <T> Boolean.yes(block: () -> T): BooleanExt<T> =//inline提升性能
     when {
