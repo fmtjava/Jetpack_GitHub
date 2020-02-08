796817c70756e87ac60642a0afe9489d811dac06
fangmingtao <fangmingtao@cias.cn>
2020-02-08 16:03:24 +0800

    使用协程简化异步代码

--- app/src/main/java/com/fmt/github/home/activity/HomeActivity.kt
+++ app/src/main/java/com/fmt/github/home/activity/HomeActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
