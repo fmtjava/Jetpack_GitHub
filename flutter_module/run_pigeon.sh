7c95a18411f0218480b2774520813cdad9bafe87
fmtjava <2694746499@qq.com>
2021-12-22 18:07:35 +0800

    接入FlutterBoost简化Native与Flutter交互

---
+++ flutter_module/run_pigeon.sh
@@ -0,0 +1,7 @@
+flutter pub run pigeon \
+  --input pigeons/message.dart \
+  --dart_out pigeons/output/pigeon.dart \
+  --objc_header_out pigeons/output/pigeon.h \
+  --objc_source_out pigeons/output/pigeon.m \
+  --java_out pigeons/output/Pigeon.java \
+  --java_package "com.example.flutter_hybird.host"
